d6e0fdbb8afaf689c5bd36bc0a1b773a6cf89e50
Wladislav ヴラド Artsimovich <git@frost.kiwi>
2022-06-12 15:04:01 +0900

    forgot to fix the skinning example file

--- example/skinning.c
+++ example/skinning.c
@@ -394,24 +394,24 @@ int main(int argc, char *argv[])
     {   /* skin */
         glEnable(GL_TEXTURE_2D);
         media.skin = image_load("../skins/gwen.png");
-        media.check = nk_subimage_id(media.skin, 512,512, nk_rect(464,32,15,15));
-        media.check_cursor = nk_subimage_id(media.skin, 512,512, nk_rect(450,34,11,11));
-        media.option = nk_subimage_id(media.skin, 512,512, nk_rect(464,64,15,15));
-        media.option_cursor = nk_subimage_id(media.skin, 512,512, nk_rect(451,67,9,9));
-        media.header = nk_subimage_id(media.skin, 512,512, nk_rect(128,0,127,24));
-        media.window = nk_subimage_id(media.skin, 512,512, nk_rect(128,23,127,104));
-        media.scrollbar_inc_button = nk_subimage_id(media.skin, 512,512, nk_rect(464,256,15,15));
-        media.scrollbar_inc_button_hover = nk_subimage_id(media.skin, 512,512, nk_rect(464,320,15,15));
-        media.scrollbar_dec_button = nk_subimage_id(media.skin, 512,512, nk_rect(464,224,15,15));
-        media.scrollbar_dec_button_hover = nk_subimage_id(media.skin, 512,512, nk_rect(464,288,15,15));
-        media.button = nk_subimage_id(media.skin, 512,512, nk_rect(384,336,127,31));
-        media.button_hover = nk_subimage_id(media.skin, 512,512, nk_rect(384,368,127,31));
-        media.button_active = nk_subimage_id(media.skin, 512,512, nk_rect(384,400,127,31));
-        media.tab_minimize = nk_subimage_id(media.skin, 512,512, nk_rect(451, 99, 9, 9));
-        media.tab_maximize = nk_subimage_id(media.skin, 512,512, nk_rect(467,99,9,9));
-        media.slider = nk_subimage_id(media.skin, 512,512, nk_rect(418,33,11,14));
-        media.slider_hover = nk_subimage_id(media.skin, 512,512, nk_rect(418,49,11,14));
-        media.slider_active = nk_subimage_id(media.skin, 512,512, nk_rect(418,64,11,14));
+        media.check = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(464,32,15,15));
+        media.check_cursor = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(450,34,11,11));
+        media.option = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(464,64,15,15));
+        media.option_cursor = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(451,67,9,9));
+        media.header = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(128,0,127,24));
+        media.window = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(128,23,127,104));
+        media.scrollbar_inc_button = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(464,256,15,15));
+        media.scrollbar_inc_button_hover = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(464,320,15,15));
+        media.scrollbar_dec_button = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(464,224,15,15));
+        media.scrollbar_dec_button_hover = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(464,288,15,15));
+        media.button = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(384,336,127,31));
+        media.button_hover = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(384,368,127,31));
+        media.button_active = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(384,400,127,31));
+        media.tab_minimize = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(451, 99, 9, 9));
+        media.tab_maximize = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(467,99,9,9));
+        media.slider = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(418,33,11,14));
+        media.slider_hover = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(418,49,11,14));
+        media.slider_active = nk_subimage_from_id(media.skin, 512,512, NK_IMAGE_STRETCH, nk_rect(418,64,11,14));
 
         /* window */
         ctx.style.window.background = nk_rgb(204,204,204);
