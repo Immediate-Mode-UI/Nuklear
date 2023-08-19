6b34b7106600612b64b8f815ace6407cffd7e28f
JS Deck <jsdeckerido@gmail.com>
2023-08-19 16:37:14 -0300

    Web colors style

---
+++ demo/common/web_colors.c
@@ -0,0 +1,240 @@
+struct nk_name_color_rgb {
+    const char *name;
+    const char *color_rgb;
+};
+
+static struct nk_name_color_rgb web_colors_name_color_rgb[] = {
+    { "aliceblue", "f0f8ff" },
+    { "antiquewhite", "faebd7" },
+    { "aqua", "00ffff" },
+    { "aquamarine", "7fffd4" },
+    { "azure", "f0ffff" },
+    { "beige", "f5f5dc" },
+    { "bisque", "ffe4c4" },
+    { "black", "000000" },
+    { "blanchedalmond", "ffebcd" },
+    { "blue", "0000ff" },
+    { "blueviolet", "8a2be2" },
+    { "brown", "a52a2a" },
+    { "burlywood", "deb887" },
+    { "cadetblue", "5f9ea0" },
+    { "chartreuse", "7fff00" },
+    { "chocolate", "d2691e" },
+    { "coral", "ff7f50" },
+    { "cornflowerblue", "6495ed" },
+    { "cornsilk", "fff8dc" },
+    { "crimson", "dc143c" },
+    { "cyan", "00ffff" },
+    { "darkblue", "00008b" },
+    { "darkcyan", "008b8b" },
+    { "darkgoldenrod", "b8860b" },
+    { "darkgray", "a9a9a9" },
+    { "darkgreen", "006400" },
+    { "darkgrey", "a9a9a9" },
+    { "darkkhaki", "bdb76b" },
+    { "darkmagenta", "8b008b" },
+    { "darkolivegreen", "556b2f" },
+    { "darkorange", "ff8c00" },
+    { "darkorchid", "9932cc" },
+    { "darkred", "8b0000" },
+    { "darksalmon", "e9967a" },
+    { "darkseagreen", "8fbc8f" },
+    { "darkslateblue", "483d8b" },
+    { "darkslategray", "2f4f4f" },
+    { "darkslategrey", "2f4f4f" },
+    { "darkturquoise", "00ced1" },
+    { "darkviolet", "9400d3" },
+    { "deeppink", "ff1493" },
+    { "deepskyblue", "00bfff" },
+    { "dimgray", "696969" },
+    { "dimgrey", "696969" },
+    { "dodgerblue", "1e90ff" },
+    { "firebrick", "b22222" },
+    { "floralwhite", "fffaf0" },
+    { "forestgreen", "228b22" },
+    { "fuchsia", "ff00ff" },
+    { "gainsboro", "dcdcdc" },
+    { "ghostwhite", "f8f8ff" },
+    { "gold", "ffd700" },
+    { "goldenrod", "daa520" },
+    { "gray", "808080" },
+    { "green", "008000" },
+    { "greenyellow", "adff2f" },
+    { "grey", "808080" },
+    { "honeydew", "f0fff0" },
+    { "hotpink", "ff69b4" },
+    { "indianred", "cd5c5c" },
+    { "indigo", "4b0082" },
+    { "ivory", "fffff0" },
+    { "khaki", "f0e68c" },
+    { "lavender", "e6e6fa" },
+    { "lavenderblush", "fff0f5" },
+    { "lawngreen", "7cfc00" },
+    { "lemonchiffon", "fffacd" },
+    { "lightblue", "add8e6" },
+    { "lightcoral", "f08080" },
+    { "lightcyan", "e0ffff" },
+    { "lightgoldenrodyellow", "fafad2" },
+    { "lightgray", "d3d3d3" },
+    { "lightgreen", "90ee90" },
+    { "lightgrey", "d3d3d3" },
+    { "lightpink", "ffb6c1" },
+    { "lightsalmon", "ffa07a" },
+    { "lightseagreen", "20b2aa" },
+    { "lightskyblue", "87cefa" },
+    { "lightslategray", "778899" },
+    { "lightslategrey", "778899" },
+    { "lightsteelblue", "b0c4de" },
+    { "lightyellow", "ffffe0" },
+    { "lime", "00ff00" },
+    { "limegreen", "32cd32" },
+    { "linen", "faf0e6" },
+    { "magenta", "ff00ff" },
+    { "maroon", "800000" },
+    { "mediumaquamarine", "66cdaa" },
+    { "mediumblue", "0000cd" },
+    { "mediumorchid", "ba55d3" },
+    { "mediumpurple", "9370db" },
+    { "mediumseagreen", "3cb371" },
+    { "mediumslateblue", "7b68ee" },
+    { "mediumspringgreen", "00fa9a" },
+    { "mediumturquoise", "48d1cc" },
+    { "mediumvioletred", "c71585" },
+    { "midnightblue", "191970" },
+    { "mintcream", "f5fffa" },
+    { "mistyrose", "ffe4e1" },
+    { "moccasin", "ffe4b5" },
+    { "navajowhite", "ffdead" },
+    { "navy", "000080" },
+    { "oldlace", "fdf5e6" },
+    { "olive", "808000" },
+    { "olivedrab", "6b8e23" },
+    { "orange", "ffa500" },
+    { "orangered", "ff4500" },
+    { "orchid", "da70d6" },
+    { "palegoldenrod", "eee8aa" },
+    { "palegreen", "98fb98" },
+    { "paleturquoise", "afeeee" },
+    { "palevioletred", "db7093" },
+    { "papayawhip", "ffefd5" },
+    { "peachpuff", "ffdab9" },
+    { "peru", "cd853f" },
+    { "pink", "ffc0cb" },
+    { "plum", "dda0dd" },
+    { "powderblue", "b0e0e6" },
+    { "purple", "800080" },
+    { "red", "ff0000" },
+    { "rosybrown", "bc8f8f" },
+    { "royalblue", "4169e1" },
+    { "saddlebrown", "8b4513" },
+    { "salmon", "fa8072" },
+    { "sandybrown", "f4a460" },
+    { "seagreen", "2e8b57" },
+    { "seashell", "fff5ee" },
+    { "sienna", "a0522d" },
+    { "silver", "c0c0c0" },
+    { "skyblue", "87ceeb" },
+    { "slateblue", "6a5acd" },
+    { "slategray", "708090" },
+    { "slategrey", "708090" },
+    { "snow", "fffafa" },
+    { "springgreen", "00ff7f" },
+    { "steelblue", "4682b4" },
+    { "tan", "d2b48c" },
+    { "teal", "008080" },
+    { "thistle", "d8bfd8" },
+    { "tomato", "ff6347" },
+    { "turquoise", "40e0d0" },
+    { "violet", "ee82ee" },
+    { "wheat", "f5deb3" },
+    { "white", "ffffff" },
+    { "whitesmoke", "f5f5f5" },
+    { "yellow", "ffff00" },
+    { "yellowgreen", "9acd32" },
+};
+
+static struct nk_map_name_color web_colors_map;
+static void
+web_colors_init(struct nk_context *ctx)
+{
+    unsigned i;
+
+    const char *(names[NK_LEN(web_colors_name_color_rgb)]);
+    struct nk_color (colors[NK_LEN(web_colors_name_color_rgb)]);
+
+    for (i = 0; i < NK_LEN(web_colors_name_color_rgb); ++i) {
+        names[i] = web_colors_name_color_rgb[i].name;
+        colors[i] = nk_rgb_hex(web_colors_name_color_rgb[i].color_rgb);
+    }
+
+    nk_map_name_color_init_colors(&web_colors_map, &ctx->memory.pool, names, colors, NK_LEN(web_colors_name_color_rgb));
+}
+
+static nk_bool
+web_colors_add(struct nk_context *ctx)
+{
+    return nk_draw_push_map_name_color(ctx, &web_colors_map);
+}
+
+static void
+web_colors_preview(struct nk_context *ctx)
+{
+    static nk_bool show_web_colors = nk_false;
+    static nk_bool wrap_text = nk_true;
+    unsigned i;
+    struct nk_color color;
+
+    if (nk_begin(ctx, "Web Colors Preview", nk_rect(50, 50, 360, 320),
+        NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|
+        NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+    {
+        static char field_buffer[256] = "Web color red = [color=\"red\"]red[/color]; web color blue = [color=#0000ff]0000ff[/color].";
+        static nk_bool field_init = nk_false;
+        static int field_len = 0;
+        char text_count[32];
+
+        if (!field_init)
+        {
+            field_init = nk_true;
+            field_len = nk_strlen(field_buffer);
+        }
+
+        nk_layout_row_static(ctx, 30, 160, 1);
+        if (nk_button_label(ctx, "Show Web Colors"))
+            show_web_colors = !show_web_colors;
+
+        nk_layout_row_static(ctx, 30, 160, 2);
+        nk_checkbox_label(ctx, "Wrap Text", &wrap_text);
+        snprintf(text_count, NK_LEN(text_count), "%d/%d", field_len, (int)NK_LEN(field_buffer) - 1);
+        nk_label(ctx, text_count, NK_TEXT_RIGHT);
+
+        nk_layout_row_static(ctx, 30, 320, 1);
+        nk_edit_string(ctx, NK_EDIT_SIMPLE, field_buffer, &field_len, NK_LEN(field_buffer), nk_filter_default);
+
+        nk_layout_row_static(ctx, 150, 320, 1);
+        nk_draw_push_color_inline(ctx, NK_COLOR_INLINE_TAG);
+        if (wrap_text)
+            nk_text_wrap(ctx, field_buffer, field_len);
+        else
+            nk_text(ctx, field_buffer, field_len, NK_TEXT_ALIGN_TOP|NK_TEXT_ALIGN_LEFT);
+        nk_draw_pop_color_inline(ctx);
+    }
+    nk_end(ctx);
+
+    if (show_web_colors)
+    {
+        if (nk_begin(ctx, "Web Colors", nk_rect(50, 50, 360, 320),
+            NK_WINDOW_BORDER|NK_WINDOW_MOVABLE|NK_WINDOW_SCALABLE|
+            NK_WINDOW_MINIMIZABLE|NK_WINDOW_TITLE))
+        {
+            for (i = 0; i < NK_LEN(web_colors_name_color_rgb); ++i) {
+                nk_layout_row_static(ctx, 20, 160, 2);
+                nk_label(ctx, web_colors_name_color_rgb[i].name, NK_TEXT_LEFT);
+                color = nk_rgb_hex(web_colors_name_color_rgb[i].color_rgb);
+                nk_label_colored(ctx, web_colors_name_color_rgb[i].color_rgb, NK_TEXT_LEFT, color);
+            }
+        }
+        nk_end(ctx);
+    }
+}
+
