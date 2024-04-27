38be74be660b7cdbd53bb5e4a589d548f9fdeda6
TheExileFox <hexile461@gmail.com>
2024-04-27 11:03:55 +0200

    Update nuklear_sdl_renderer.h
    change SDL include path to the recommended path

--- demo/sdl_renderer/nuklear_sdl_renderer.h
+++ demo/sdl_renderer/nuklear_sdl_renderer.h
@@ -11,7 +11,7 @@
 #ifndef NK_SDL_RENDERER_H_
 #define NK_SDL_RENDERER_H_
 
-#include <SDL2/SDL.h>
+#include <SDL.h>
 NK_API struct nk_context*   nk_sdl_init(SDL_Window *win, SDL_Renderer *renderer);
 NK_API void                 nk_sdl_font_stash_begin(struct nk_font_atlas **atlas);
 NK_API void                 nk_sdl_font_stash_end(void);
