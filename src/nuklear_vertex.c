156d85d7dc97777a805c0c74e4b9f015ac4b01da
ryuukk <ryuukk.dev@gmail.com>
2024-09-25 16:18:54 +0200

    Shrink the w/h by the whole thickness

--- src/nuklear_vertex.c
+++ src/nuklear_vertex.c
@@ -979,9 +979,9 @@ nk_draw_list_stroke_rect(struct nk_draw_list *list, struct nk_rect rect,
         */
         float hl = nk_div_round_closest(thickness, 2.0);
         rect.x += hl;
-        rect.w -= hl;
+        rect.w -= thickness;
         rect.y += hl;
-        rect.h -= hl;
+        rect.h -= thickness;
         nk_draw_list_path_rect_to(list, nk_vec2(rect.x, rect.y),
             nk_vec2(rect.x + rect.w, rect.y + rect.h), rounding);
     } else {
