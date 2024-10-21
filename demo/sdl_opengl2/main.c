b6a7791d13052315b7698013cb7cd0e7bc1e6113
Alexander W. Schultz <alexschultz002@gmail.com>
2024-10-20 23:17:02 -0400

    disables intrinsics on SDL demos
    SDL library has an issue where it expects intrinsics headers to be
    provided, but intrinsics are not exactly part of a standard, so there is
    no guarantee that they will be available. As such, the library allows
    you to turn them off. I am disabling them to allow for better
    portability and ease of compilation.

--- demo/sdl_opengl2/main.c
+++ demo/sdl_opengl2/main.c
@@ -34,7 +34,7 @@
  * ===============================================================*/
 /* This are some code examples to provide a small overview of what can be
  * done with this library. To try out an example uncomment the defines */
-/*#define INCLUDE_ALL */
+#define INCLUDE_ALL 
 /*#define INCLUDE_STYLE */
 /*#define INCLUDE_CALCULATOR */
 /*#define INCLUDE_CANVAS */
