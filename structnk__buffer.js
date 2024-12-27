0b63785b39c7969a232513a3b8f97d36a9ecae3b
RobLoach <RobLoach@users.noreply.github.com>
2024-12-27 05:05:38 +0000

    deploy: 7f406eea126506925d012fb3c1d9b9a9002a99e4

---
+++ structnk__buffer.js
@@ -0,0 +1,11 @@
+var structnk__buffer =
+[
+    [ "allocated", "structnk__buffer.html#a91e9be62aa08687400bc00059825de02", null ],
+    [ "calls", "structnk__buffer.html#acd962a6e042a8ffabac679768e4851bb", null ],
+    [ "grow_factor", "structnk__buffer.html#ab4ec59165f6aa6e9358bced8070cc84e", null ],
+    [ "memory", "structnk__buffer.html#a228b585debec1d328859fb52080ca3fd", null ],
+    [ "needed", "structnk__buffer.html#a6b0ea49209eaba5285b715d902c5f446", null ],
+    [ "pool", "structnk__buffer.html#a0cd2b90bb2994190fa1aa0e9ffbc1184", null ],
+    [ "size", "structnk__buffer.html#a71e66eb6dad2c5827e363f2389ad4505", null ],
+    [ "type", "structnk__buffer.html#a3842a03554db557944e84bc5af61249e", null ]
+];
