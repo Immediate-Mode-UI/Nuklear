f45f081fcc366ac2b63aa45f01f741b43c11a11b
RobLoach <RobLoach@users.noreply.github.com>
2025-03-04 23:37:54 +0000

    deploy: 7408af0b8214fbef730d6c2c65537e3f23bfe1a2

---
+++ navtreedata.js
@@ -0,0 +1,113 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Nuklear", "index.html", [
+    [ "Contents", "index.html#autotoc_md1", null ],
+    [ "About", "index.html#autotoc_md2", null ],
+    [ "Highlights", "index.html#autotoc_md3", null ],
+    [ "Features", "index.html#autotoc_md4", null ],
+    [ "Usage", "index.html#autotoc_md5", [
+      [ "Flags", "index.html#autotoc_md6", null ],
+      [ "Constants", "index.html#autotoc_md7", null ],
+      [ "Dependencies", "index.html#autotoc_md8", null ]
+    ] ],
+    [ "Example", "index.html#autotoc_md9", null ],
+    [ "API", "index.html#autotoc_md10", null ],
+    [ "Context", "Context.html", [
+      [ "Usage", "Context.html#autotoc_md11", null ],
+      [ "Reference", "Context.html#autotoc_md12", null ]
+    ] ],
+    [ "Input", "Input.html", [
+      [ "Usage", "Input.html#autotoc_md15", null ],
+      [ "Reference", "Input.html#autotoc_md16", null ]
+    ] ],
+    [ "Drawing", "Drawing.html", [
+      [ "Usage", "Drawing.html#autotoc_md17", null ],
+      [ "Reference", "Drawing.html#autotoc_md18", null ]
+    ] ],
+    [ "Window", "Window.html", [
+      [ "Usage", "Window.html#autotoc_md19", null ],
+      [ "Reference", "Window.html#autotoc_md20", null ],
+      [ "nk_panel_flags", "Window.html#autotoc_md21", null ],
+      [ "nk_collapse_states", "Window.html#autotoc_md22", null ]
+    ] ],
+    [ "Layouting", "Layouting.html", [
+      [ "Usage", "Layouting.html#autotoc_md57", null ],
+      [ "Reference", "Layouting.html#autotoc_md58", null ]
+    ] ],
+    [ "Groups", "Groups.html", [
+      [ "Usage", "Groups.html#autotoc_md73", null ],
+      [ "Reference", "Groups.html#autotoc_md74", null ]
+    ] ],
+    [ "Tree", "Tree.html", [
+      [ "Usage", "Tree.html#autotoc_md81", null ],
+      [ "Reference", "Tree.html#autotoc_md82", null ],
+      [ "nk_tree_type", "Tree.html#autotoc_md83", null ]
+    ] ],
+    [ "Properties", "Properties.html", [
+      [ "Usage", "Properties.html#autotoc_md94", null ],
+      [ "Reference", "Properties.html#autotoc_md95", null ],
+      [ "# nk_property_int", "Properties.html#autotoc_md96", null ]
+    ] ],
+    [ "Font", "Font.html", [
+      [ "Using your own implementation without vertex buffer output", "Font.html#autotoc_md102", null ],
+      [ "Using your own implementation with vertex buffer output", "Font.html#autotoc_md103", null ],
+      [ "Nuklear font baker", "Font.html#autotoc_md104", null ]
+    ] ],
+    [ "Buffer", "Memory.html", null ],
+    [ "Editor", "Text.html", null ],
+    [ "Stack", "Stack.html", [
+      [ "Stack", "Stack.html#autotoc_md105", null ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"Context.html",
+"structnk__buffer.html#a71e66eb6dad2c5827e363f2389ad4505"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
