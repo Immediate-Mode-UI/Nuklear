5987191a164adeb1fc25e2f1638fa93c50645d78
PavelSharp <pavel.ivonov@icloud.com>
2025-10-18 02:03:43 +0300

    fix: interpret len as byte count in nk_str_insert_text_char and nk_str_insert_str_char

--- src/nuklear_string.c
+++ src/nuklear_string.c
@@ -167,12 +167,12 @@ nk_str_insert_at_rune(struct nk_str *str, int pos, const char *cstr, int len)
 NK_API int
 nk_str_insert_text_char(struct nk_str *str, int pos, const char *text, int len)
 {
-    return nk_str_insert_text_utf8(str, pos, text, len);
+    return nk_str_insert_at_rune(str, pos, text, len) ? len : 0;
 }
 NK_API int
 nk_str_insert_str_char(struct nk_str *str, int pos, const char *text)
 {
-    return nk_str_insert_text_utf8(str, pos, text, nk_strlen(text));
+    return nk_str_insert_text_char(str, pos, text, nk_strlen(text));
 }
 NK_API int
 nk_str_insert_text_utf8(struct nk_str *str, int pos, const char *text, int len)
