0d62612da12fae269a9bd45796c6c6d33213cf13
sleeptightAnsiC <91839286+sleeptightAnsiC@users.noreply.github.com>
2026-02-17 11:20:14 +0100

    feat(tooltip): add new variation of tooltip with offset
    Adds new variation of nk_tooltip_* that let's you specify the relative
    position to the cursor and the offset. This was done in order to prevent
    cursor from covering the content of tooltip. The default tooltip should
    now provide more sane offset (still not perfect!) Also, added a tooltip
    editor to demo overview in order to test it. Parts of this code were
    originally written by Robert Winkler (thanks!)
    
    Origin: https://github.com/Immediate-Mode-UI/Nuklear/pull/881
    Co-authored-by: Robert Winkler <rob121618@gmail.com>

--- nuklear.h
+++ nuklear.h
@@ -3826,12 +3826,27 @@ NK_API void nk_contextual_end(struct nk_context*);
  *                                  TOOLTIP
  *
  * ============================================================================= */
+enum nk_tooltip_flags {
+    /**!< tells where tooltip should appear relatively to the cursor, can be combined (e.g. BELOW|ON_RIGHT) */
+    NK_TOOLTIP_ABOVE       = NK_FLAG(0),
+    NK_TOOLTIP_BELOW       = NK_FLAG(1),
+    NK_TOOLTIP_ON_LEFT     = NK_FLAG(2),
+    NK_TOOLTIP_ON_RIGHT    = NK_FLAG(3),
+    /**!< if set, the offset will be absolute, instead of relative */
+    NK_TOOLTIP_ABS_OFFSET  = NK_FLAG(4)
+    /* FIXME: https://github.com/Immediate-Mode-UI/Nuklear/issues/899 */
+    /*NK_TOOLTIP_CLAMP_IN_SCREEN = NK_FLAG(5),*/
+};
 NK_API void nk_tooltip(struct nk_context*, const char*);
+NK_API void nk_tooltip_offset(struct nk_context *ctx, const char *text, nk_flags tooltip, struct nk_vec2);
 #ifdef NK_INCLUDE_STANDARD_VARARGS
 NK_API void nk_tooltipf(struct nk_context*, NK_PRINTF_FORMAT_STRING const char*, ...) NK_PRINTF_VARARG_FUNC(2);
 NK_API void nk_tooltipfv(struct nk_context*, NK_PRINTF_FORMAT_STRING const char*, va_list) NK_PRINTF_VALIST_FUNC(2);
+NK_API void nk_tooltipf_offset(struct nk_context*, nk_flags tooltip, struct nk_vec2, NK_PRINTF_FORMAT_STRING const char*, ...) NK_PRINTF_VARARG_FUNC(4);
+NK_API void nk_tooltipfv_offset(struct nk_context*, nk_flags tooltip, struct nk_vec2, NK_PRINTF_FORMAT_STRING const char*, va_list) NK_PRINTF_VALIST_FUNC(4);
 #endif
 NK_API nk_bool nk_tooltip_begin(struct nk_context*, float width);
+NK_API nk_bool nk_tooltip_begin_offset(struct nk_context*, float width, nk_flags tooltip, struct nk_vec2);
 NK_API void nk_tooltip_end(struct nk_context*);
 /* =============================================================================
  *
@@ -30603,10 +30618,40 @@ nk_combobox_callback(struct nk_context *ctx,
  *                              TOOLTIP
  *
  * ===============================================================*/
+NK_LIB struct nk_vec2
+nk_tooltip_get_default_offset(const struct nk_context *ctx)
+{
+    struct nk_vec2 offset = {0};
+    NK_ASSERT(ctx);
+    if (!ctx) return offset;
+    if (ctx->style.cursor_active) {
+        /* nuklear is drawing its own cursor so we can reuse its size (best case!) */
+        offset.x = ctx->style.window.padding.x + ctx->style.cursor_active->size.x;
+        offset.x = ctx->style.window.padding.y + ctx->style.cursor_active->size.y;
+    } else if (ctx->style.font) {
+        /* assume that cursor size is similar to font height (flawed but reasonable)*/
+        offset.y = ctx->style.window.padding.x + ctx->style.font->height;
+        offset.x = ctx->style.window.padding.y + ctx->style.font->height;
+    }
+    return offset;
+}
+NK_LIB nk_flags
+nk_tooltip_get_default_flags(const struct nk_context *ctx)
+{
+    NK_UNUSED(ctx);
+    return NK_TOOLTIP_BELOW|NK_TOOLTIP_ON_RIGHT;
+}
 NK_API nk_bool
 nk_tooltip_begin(struct nk_context *ctx, float width)
 {
-    int x,y,w,h;
+    return nk_tooltip_begin_offset(ctx, width,
+                                   nk_tooltip_get_default_flags(ctx),
+                                   nk_tooltip_get_default_offset(ctx));
+}
+NK_API nk_bool
+nk_tooltip_begin_offset(struct nk_context *ctx, float width, nk_flags tooltip, struct nk_vec2 offset)
+{
+    int x,y,w,h,mul_x,mul_y;
     struct nk_window *win;
     const struct nk_input *in;
     struct nk_rect bounds;
@@ -30625,14 +30670,38 @@ nk_tooltip_begin(struct nk_context *ctx, float width)
         return 0;
 
     w = nk_iceilf(width);
-    h = nk_iceilf(nk_null_rect.h);
-    x = nk_ifloorf(in->mouse.pos.x + 1) - (int)win->layout->clip.x;
-    y = nk_ifloorf(in->mouse.pos.y + 1) - (int)win->layout->clip.y;
+    h = ctx->current->layout->row.min_height;
+
+    /* find axis multipliers based on bitmask state */
+    mul_x = 0;
+    mul_x -= !!(tooltip & NK_TOOLTIP_ON_LEFT );
+    mul_x += !!(tooltip & NK_TOOLTIP_ON_RIGHT);
+    NK_ASSERT(mul_x == -1 || mul_x == 0 || mul_x == 1);
+    mul_y = 0;
+    mul_y -= !!(tooltip & NK_TOOLTIP_ABOVE);
+    mul_y += !!(tooltip & NK_TOOLTIP_BELOW);
+    NK_ASSERT(mul_y == -1 || mul_y == 0 || mul_y == 1);
+
+    /* turn relative offset into absolute, unless it's already absolute
+     * notice that offset axis is ignored in cases where mul==0
+     * (if you don't like this behavior, make sure to use ABS_OFFSET flag)*/
+    if (!(tooltip & NK_TOOLTIP_ABS_OFFSET)) {
+        offset.x *= mul_x;
+        offset.y *= mul_y;
+    }
+
+    /* find origin */
+    x = -w/2 + (mul_x * w/2);
+    x += nk_ifloorf(in->mouse.pos.x + 1) - (int)win->layout->clip.x;
+    x += offset.x;
+    y = -h/2 + (mul_y * h/2);
+    y += nk_ifloorf(in->mouse.pos.y + 1) - (int)win->layout->clip.y;
+    y += offset.y;
 
     bounds.x = (float)x;
     bounds.y = (float)y;
     bounds.w = (float)w;
-    bounds.h = (float)h;
+    bounds.h = (float)nk_iceilf(nk_null_rect.h);
 
     ret = nk_popup_begin(ctx, NK_POPUP_DYNAMIC,
         "__##Tooltip##__", NK_WINDOW_NO_SCROLLBAR|NK_WINDOW_BORDER, bounds);
@@ -30641,7 +30710,6 @@ nk_tooltip_begin(struct nk_context *ctx, float width)
     ctx->current->layout->type = NK_PANEL_TOOLTIP;
     return ret;
 }
-
 NK_API void
 nk_tooltip_end(struct nk_context *ctx)
 {
@@ -30653,7 +30721,7 @@ nk_tooltip_end(struct nk_context *ctx)
     nk_popup_end(ctx);
 }
 NK_API void
-nk_tooltip(struct nk_context *ctx, const char *text)
+nk_tooltip_offset(struct nk_context *ctx, const char *text, nk_flags tooltip, struct nk_vec2 offset)
 {
     const struct nk_style *style;
     struct nk_vec2 padding;
@@ -30681,13 +30749,28 @@ nk_tooltip(struct nk_context *ctx, const char *text)
     text_height = (style->font->height + 2 * padding.y);
 
     /* execute tooltip and fill with text */
-    if (nk_tooltip_begin(ctx, (float)text_width)) {
+    if (nk_tooltip_begin_offset(ctx, (float)text_width, tooltip, offset)) {
         nk_layout_row_dynamic(ctx, (float)text_height, 1);
         nk_text(ctx, text, text_len, NK_TEXT_LEFT);
         nk_tooltip_end(ctx);
     }
 }
+NK_API void
+nk_tooltip(struct nk_context *ctx, const char *text)
+{
+    nk_tooltip_offset(ctx, text,
+                      nk_tooltip_get_default_flags(ctx),
+                      nk_tooltip_get_default_offset(ctx));
+}
 #ifdef NK_INCLUDE_STANDARD_VARARGS
+NK_API void
+nk_tooltipf_offset(struct nk_context *ctx, nk_flags tooltip, struct nk_vec2 offset, const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    nk_tooltipfv_offset(ctx, tooltip, offset, fmt, args);
+    va_end(args);
+}
 NK_API void
 nk_tooltipf(struct nk_context *ctx, const char *fmt, ...)
 {
@@ -30695,6 +30778,13 @@ nk_tooltipf(struct nk_context *ctx, const char *fmt, ...)
     va_start(args, fmt);
     nk_tooltipfv(ctx, fmt, args);
     va_end(args);
+}
+NK_API void
+nk_tooltipfv_offset(struct nk_context *ctx, nk_flags tooltip, struct nk_vec2 offset, const char *fmt, va_list args)
+{
+    char buf[256];
+    nk_strfmt(buf, NK_LEN(buf), fmt, args);
+    nk_tooltip_offset(ctx, buf, tooltip, offset);
 }
 NK_API void
 nk_tooltipfv(struct nk_context *ctx, const char *fmt, va_list args)
@@ -30762,6 +30852,8 @@ nk_tooltipfv(struct nk_context *ctx, const char *fmt, va_list args)
 ///   - [y]: Minor version with non-breaking API and library changes
 ///   - [z]: Patch version with no direct changes to the API
 ///
+/// - 2026/02/17 (4.13.3) - Fixed default tooltip from being covered by the cursor,
+///                         added new tooltip variation that allows to set the offset manually
 /// - 2026/01/31 (4.13.2) - Fix: replace incorrect static asserts for size(nk_bool)
 /// - 2026/01/26 (4.13.1) - Fix: nk_do_property now uses NK_STRTOD via macro
 ///                       - Fix: failure to build from source, due to
