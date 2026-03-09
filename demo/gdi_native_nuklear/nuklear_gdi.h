5db93e1f052b5e4b7b0602fc71eabc68d5456b08
Rob Loach <RobLoach@users.noreply.github.com>
2026-03-08 20:04:49 -0400

    Update context from d3d9 to gdi for XBUTTON events

--- demo/gdi_native_nuklear/nuklear_gdi.h
+++ demo/gdi_native_nuklear/nuklear_gdi.h
@@ -865,10 +865,10 @@ nk_gdi_handle_event(nk_gdi_ctx gdi, HWND wnd, UINT msg, WPARAM wparam, LPARAM lp
     case WM_XBUTTONDOWN:
         switch (GET_XBUTTON_WPARAM(wparam)) {
         case XBUTTON1:
-            nk_input_button(&d3d9.ctx, NK_BUTTON_X1, (short)LOWORD(lparam), (short)HIWORD(lparam), 1);
+            nk_input_button(&gdi->ctx, NK_BUTTON_X1, (short)LOWORD(lparam), (short)HIWORD(lparam), 1);
             break;
         case XBUTTON2:
-            nk_input_button(&d3d9.ctx, NK_BUTTON_X2, (short)LOWORD(lparam), (short)HIWORD(lparam), 1);
+            nk_input_button(&gdi->ctx, NK_BUTTON_X2, (short)LOWORD(lparam), (short)HIWORD(lparam), 1);
             break;
         }
         SetCapture(wnd);
@@ -877,10 +877,10 @@ nk_gdi_handle_event(nk_gdi_ctx gdi, HWND wnd, UINT msg, WPARAM wparam, LPARAM lp
     case WM_XBUTTONUP:
         switch (GET_XBUTTON_WPARAM(wparam)) {
         case XBUTTON1:
-            nk_input_button(&d3d9.ctx, NK_BUTTON_X1, (short)LOWORD(lparam), (short)HIWORD(lparam), 0);
+            nk_input_button(&gdi->ctx, NK_BUTTON_X1, (short)LOWORD(lparam), (short)HIWORD(lparam), 0);
             break;
         case XBUTTON2:
-            nk_input_button(&d3d9.ctx, NK_BUTTON_X2, (short)LOWORD(lparam), (short)HIWORD(lparam), 0);
+            nk_input_button(&gdi->ctx, NK_BUTTON_X2, (short)LOWORD(lparam), (short)HIWORD(lparam), 0);
             break;
         }
         ReleaseCapture();
