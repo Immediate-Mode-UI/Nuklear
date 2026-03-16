7002929a46deebf66b9545b860ad159beede699f
Robert Winkler <rob121618@gmail.com>
2026-03-16 14:42:46 -0700

    Remove redundant nk_zero_struct() calls

--- nuklear.h
+++ nuklear.h
@@ -19896,7 +19896,6 @@ nk_create_table(struct nk_context *ctx)
     struct nk_page_element *elem;
     elem = nk_create_page_element(ctx);
     if (!elem) return 0;
-    nk_zero_struct(*elem);
     return &elem->data.tbl;
 }
 NK_LIB void
@@ -19986,7 +19985,6 @@ nk_create_panel(struct nk_context *ctx)
     struct nk_page_element *elem;
     elem = nk_create_page_element(ctx);
     if (!elem) return 0;
-    nk_zero_struct(*elem);
     return &elem->data.pan;
 }
 NK_LIB void
