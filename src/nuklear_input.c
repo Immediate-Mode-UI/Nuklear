21ea106963447b085d6863e4a253c51398e765b9
Robert Winkler <rob121618@gmail.com>
2026-04-20 12:46:25 -0700

    Add nk_input_is_mouse_hovering_delay_rect() to implement delayed popups

--- src/nuklear_input.c
+++ src/nuklear_input.c
@@ -218,6 +218,19 @@ nk_input_is_mouse_hovering_rect(const struct nk_input *i, struct nk_rect rect)
     return NK_INBOX(i->mouse.pos.x, i->mouse.pos.y, rect.x, rect.y, rect.w, rect.h);
 }
 NK_API nk_bool
+nk_input_is_mouse_hovering_delay_rect(const struct nk_context *ctx, struct nk_rect rect, float* timer, float delay)
+{
+    NK_ASSERT(ctx);
+    if (!ctx) return nk_false;
+    if (NK_INBOX(ctx->input.mouse.pos.x, ctx->input.mouse.pos.y, rect.x, rect.y, rect.w, rect.h)) {
+        *timer += ctx->delta_time_seconds;
+    } else {
+        *timer = 0;
+    }
+    return *timer >= delay;
+
+}
+NK_API nk_bool
 nk_input_is_mouse_prev_hovering_rect(const struct nk_input *i, struct nk_rect rect)
 {
     if (!i) return nk_false;
