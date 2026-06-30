Immediate-Mode-UI/Nuklear

commit 063968520f7bb8129b7a164906be055da3634cd3

demo: Quality of Life updates
- Meaningful titles across all Demos
- Switch to CTRL+Q to exit the demos

demo/gdi/main.c
@@ -129,9 +129,11 @@ int main(void)
         MSG msg;
         nk_input_begin(ctx);
         if (needs_refresh == 0) {
-            if (GetMessageW(&msg, NULL, 0, 0) <= 0)
+            if (GetMessageW(&msg, NULL, 0, 0) <= 0) {
                 running = 0;
-            else {
+            } else if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
+            } else {
                 TranslateMessage(&msg);
                 DispatchMessageW(&msg);
             }

demo/allegro5/main.c
@@ -100,6 +100,7 @@ int main(void)
     al_set_mouse_wheel_precision(150);
     al_install_keyboard();
 
+    al_set_new_window_title("allegro5");
     al_set_new_display_flags(ALLEGRO_WINDOWED|ALLEGRO_RESIZABLE|ALLEGRO_OPENGL);
     al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
     al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
@@ -133,8 +134,11 @@ int main(void)
 
         get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
 
-        if (get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            break;
+        if (get_event) {
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE ||
+                (ev.keyboard.keycode == ALLEGRO_KEY_Q && ev.keyboard.modifiers & ALLEGRO_KEYMOD_CTRL)) {
+                break;
+            }
         }
 
         /* Very Important: Always do nk_input_begin / nk_input_end even if

demo/d3d11/main.c
@@ -228,6 +228,8 @@ int main(void)
         {
             if (msg.message == WM_QUIT)
                 running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
         }

demo/d3d12/main.c
@@ -1,7 +1,7 @@
 /* nuklear - 1.32.0 - public domain */
 
 /*
- *   === IMPORTATE NOTE FOR D3D12 ===  
+ *   === IMPORTATE NOTE FOR D3D12 ===
  *
  * Due to a bug in the Windows SDK that
  * was fixed in version 10.0.22000.0 the
@@ -9,7 +9,7 @@
  * a minimum! Compiling with a lower version
  * will result in compiler warnings and a
  * crashing application!
- * 
+ *
  */
 
 #define COBJMACROS
@@ -332,6 +332,8 @@ int main(void)
         while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
         {
             if (msg.message == WM_QUIT)
+                running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
                 running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);

demo/d3d9/main.c
@@ -233,6 +233,8 @@ int main(void)
         while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_QUIT)
                 running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
         }

demo/gdip/main.c
@@ -136,6 +136,8 @@ int main(void)
         while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_QUIT)
                 running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
             needs_refresh = 1;

demo/glfw_opengl2/main.c
@@ -110,7 +110,7 @@ int main(void)
         fprintf(stdout, "[GFLW] failed to init!\n");
         exit(1);
     }
-    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Demo", NULL, NULL);
+    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "glfw_opengl2", NULL, NULL);
     glfwMakeContextCurrent(win);
     glfwGetWindowSize(win, &width, &height);
 
@@ -154,6 +154,11 @@ int main(void)
     {
         /* Input */
         glfwPollEvents();
+        if (glfwGetKey(win, GLFW_KEY_Q) == GLFW_PRESS &&
+             (glfwGetKey(win, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS ||
+              glfwGetKey(win, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)) {
+            glfwSetWindowShouldClose(win, nk_true);
+        }
         nk_glfw3_new_frame();
 
         /* GUI */

demo/glfw_opengl3/main.c
@@ -107,7 +107,7 @@ int main(void)
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Demo", NULL, NULL);
+    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "glfw_opengl3", NULL, NULL);
     glfwMakeContextCurrent(win);
     glfwGetWindowSize(win, &width, &height);
 
@@ -139,6 +139,11 @@ int main(void)
     {
         /* Input */
         glfwPollEvents();
+        if (glfwGetKey(win, GLFW_KEY_Q) == GLFW_PRESS &&
+             (glfwGetKey(win, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS ||
+              glfwGetKey(win, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)) {
+            glfwSetWindowShouldClose(win, nk_true);
+        }
         nk_glfw3_new_frame(&glfw);
 
         /* GUI */

commit a7353204d0f76fa38df9e9a2c02247b52c3bc878

fix: demo/gdi/main.c not compiling due to missing brace
This file was failing with syntax error which appears to be broken since
https://github.com/Immediate-Mode-UI/Nuklear/commit/063968520f7bb8129b7a164906be055da3634cd3
I suspect none of the Windows-specific demos were actually tested.

Refs: https://github.com/Immediate-Mode-UI/Nuklear/pull/937

demo/gdi/main.c
@@ -131,7 +131,7 @@ int main(void)
         if (needs_refresh == 0) {
             if (GetMessageW(&msg, NULL, 0, 0) <= 0) {
                 running = 0;
-            } else if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+            } else if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15)) {
                 running = 0;
             } else {
                 TranslateMessage(&msg);