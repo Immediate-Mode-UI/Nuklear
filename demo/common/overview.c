Immediate-Mode-UI/Nuklear

commit 063968520f7bb8129b7a164906be055da3634cd3

demo: Quality of Life updates
- Meaningful titles across all Demos
- Switch to CTRL+Q to exit the demos

demo/common/overview.c
@@ -890,7 +890,7 @@ overview(struct nk_context *ctx)
                     "BOTTOM_CENTER",
                     "BOTTOM_RIGHT"
                 };
-                static int cur_pos = NK_TOP_LEFT;
+                int cur_pos = NK_TOP_LEFT;
 
                 if (!text_initialized) {
                     const char text_default[] = "you can customize this!";
@@ -902,7 +902,7 @@ overview(struct nk_context *ctx)
                 bounds = nk_widget_bounds(ctx);
                 nk_label(ctx, "Hover for custom tooltip (you can customize it below)", NK_TEXT_LEFT);
                 if (nk_input_is_mouse_hovering_rect(in, bounds)) {
-                    nk_tooltip_offset(ctx, text_buf, cur_pos, offset);
+                    nk_tooltip_offset(ctx, text_buf, (enum nk_tooltip_pos)cur_pos, offset);
                 }
                 nk_layout_row_dynamic(ctx, 1, 1);
                 nk_rule_horizontal(ctx, nk_white, nk_true);
@@ -1445,11 +1445,11 @@ overview(struct nk_context *ctx)
             nk_layout_row_dynamic(ctx, 20, 2);
             for (i = 0; i < NK_BUTTON_MAX; i++) {
                 nk_label(ctx, button_names[i], NK_TEXT_LEFT);
-                if (nk_input_is_mouse_pressed(in, i))
+                if (nk_input_is_mouse_pressed(in, (enum nk_buttons)i))
                     nk_label(ctx, "Pressed", NK_TEXT_LEFT);
-                else if (nk_input_is_mouse_down(in, i))
+                else if (nk_input_is_mouse_down(in, (enum nk_buttons)i))
                     nk_label(ctx, "Down", NK_TEXT_LEFT);
-                else if (nk_input_is_mouse_released(in, i))
+                else if (nk_input_is_mouse_released(in, (enum nk_buttons)i))
                     nk_label(ctx, "Released", NK_TEXT_LEFT);
                 else
                     nk_label(ctx, "Up", NK_TEXT_LEFT);

demo/allegro5/main.c
@@ -100,6 +100,7 @@ int main(void)
     al_set_mouse_wheel_precision(150);
     al_install_keyboard();
 
+    al_set_new_window_title("allegro5");
     al_set_new_display_flags(ALLEGRO_WINDOWED|ALLEGRO_RESIZABLE|ALLEGRO_OPENGL);
     al_set_new_display_option(ALLEGRO_SAMPLE_BUFFERS, 1, ALLEGRO_SUGGEST);
     al_set_new_display_option(ALLEGRO_SAMPLES, 8, ALLEGRO_SUGGEST);
@@ -133,8 +134,11 @@ int main(void)
 
         get_event = al_wait_for_event_until(event_queue, &ev, &timeout);
 
-        if (get_event && ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            break;
+        if (get_event) {
+            if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE ||
+                (ev.keyboard.keycode == ALLEGRO_KEY_Q && ev.keyboard.modifiers & ALLEGRO_KEYMOD_CTRL)) {
+                break;
+            }
         }
 
         /* Very Important: Always do nk_input_begin / nk_input_end even if

demo/d3d11/main.c
@@ -228,6 +228,8 @@ int main(void)
         {
             if (msg.message == WM_QUIT)
                 running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
         }

demo/d3d12/main.c
@@ -1,7 +1,7 @@
 /* nuklear - 1.32.0 - public domain */
 
 /*
- *   === IMPORTATE NOTE FOR D3D12 ===  
+ *   === IMPORTATE NOTE FOR D3D12 ===
  *
  * Due to a bug in the Windows SDK that
  * was fixed in version 10.0.22000.0 the
@@ -9,7 +9,7 @@
  * a minimum! Compiling with a lower version
  * will result in compiler warnings and a
  * crashing application!
- * 
+ *
  */
 
 #define COBJMACROS
@@ -332,6 +332,8 @@ int main(void)
         while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
         {
             if (msg.message == WM_QUIT)
+                running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
                 running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);

demo/d3d9/main.c
@@ -233,6 +233,8 @@ int main(void)
         while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_QUIT)
                 running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
         }

demo/gdi/main.c
@@ -129,9 +129,11 @@ int main(void)
         MSG msg;
         nk_input_begin(ctx);
         if (needs_refresh == 0) {
-            if (GetMessageW(&msg, NULL, 0, 0) <= 0)
+            if (GetMessageW(&msg, NULL, 0, 0) <= 0) {
                 running = 0;
-            else {
+            } else if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
+            } else {
                 TranslateMessage(&msg);
                 DispatchMessageW(&msg);
             }

demo/gdip/main.c
@@ -136,6 +136,8 @@ int main(void)
         while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE)) {
             if (msg.message == WM_QUIT)
                 running = 0;
+            if (msg.message == WM_SYSKEYDOWN && msg.wParam == 'Q' && GetKeyState(VK_CONTROL) & (1 << 15))
+                running = 0;
             TranslateMessage(&msg);
             DispatchMessageW(&msg);
             needs_refresh = 1;

demo/glfw_opengl2/main.c
@@ -110,7 +110,7 @@ int main(void)
         fprintf(stdout, "[GFLW] failed to init!\n");
         exit(1);
     }
-    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Demo", NULL, NULL);
+    win = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "glfw_opengl2", NULL, NULL);
     glfwMakeContextCurrent(win);
     glfwGetWindowSize(win, &width, &height);
 
@@ -154,6 +154,11 @@ int main(void)
     {
         /* Input */
         glfwPollEvents();
+        if (glfwGetKey(win, GLFW_KEY_Q) == GLFW_PRESS &&
+             (glfwGetKey(win, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS ||
+              glfwGetKey(win, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS)) {
+            glfwSetWindowShouldClose(win, nk_true);
+        }
         nk_glfw3_new_frame();
 
         /* GUI */

commit 9df1c7c63fea5041d6dae5b3eec26eb5b57c9552

fix(overview): custom tooltip position was impossible to select + use proper nk_edit
For some reason the 'static' was missing from cur_pos preventing
selection of custom tooltip position. This was broken since:
https://github.com/Immediate-Mode-UI/Nuklear/commit/063968520f7bb8129b7a164906be055da3634cd3
Also fixed TODO note by using nk_edit_string_zero_terminated rather
than reimplementing the wheel with text_len and '\0'

Refs: https://github.com/Immediate-Mode-UI/Nuklear/pull/900
Refs: https://github.com/Immediate-Mode-UI/Nuklear/pull/937

demo/common/overview.c
@@ -904,7 +904,6 @@ overview(struct nk_context *ctx)
             /* editor for custom tooltip */
             {
                 static char text_buf[64] = {0};
-                static int text_len = 0;
                 static int text_initialized = 0;
                 static struct nk_vec2 offset = {0};
                 static const char* tooltip_positions[] =
@@ -921,13 +920,12 @@ overview(struct nk_context *ctx)
                     "BOTTOM_CENTER",
                     "BOTTOM_RIGHT"
                 };
-                int cur_pos = NK_TOP_LEFT;
+                static int cur_pos = NK_TOP_LEFT;
 
                 if (!text_initialized) {
                     const char text_default[] = "you can customize this!";
                     NK_ASSERT(sizeof(text_default) < sizeof(text_buf));
                     memcpy(text_buf, text_default, sizeof(text_default));
-                    text_len = sizeof(text_default) - 1;
                     text_initialized = 1;
                 }
                 bounds = nk_widget_bounds(ctx);
@@ -939,17 +937,16 @@ overview(struct nk_context *ctx)
                 nk_rule_horizontal(ctx, nk_white, nk_true);
                 nk_layout_row_dynamic(ctx, 30, 2);
                 nk_label(ctx, "custom tooltip text:", NK_TEXT_LEFT);
-                nk_edit_string(ctx, NK_EDIT_FIELD, text_buf, &text_len, sizeof(text_buf), nk_filter_default);
-                text_buf[text_len] = '\0';  /* TODO: why nk_edit_string is NOT setting this on its own? */
+                nk_edit_string_zero_terminated(ctx, NK_EDIT_SIMPLE, text_buf, sizeof(text_buf), nk_filter_default);
                 nk_layout_row_dynamic(ctx, 30, 1);
                 cur_pos = nk_combo(ctx, tooltip_positions, NK_LEN(tooltip_positions), cur_pos, 25, nk_vec2(200, 200));
 
 
                 nk_layout_row_dynamic(ctx, 30, 2);
                 nk_label(ctx, "custom tooltip offset", NK_TEXT_LEFT);
-                nk_property_float(ctx, "x", -100.0f, &offset.x, 100.0f, 5.0f, 0.5f);
+                nk_property_float(ctx, "#x", -100.0f, &offset.x, 100.0f, 5.0f, 0.5f);
                 nk_label(ctx, "custom tooltip offset", NK_TEXT_LEFT);
-                nk_property_float(ctx, "y", -100.0f, &offset.y, 100.0f, 5.0f, 0.5f);
+                nk_property_float(ctx, "#y", -100.0f, &offset.y, 100.0f, 5.0f, 0.5f);
             }
 
             nk_tree_pop(ctx);